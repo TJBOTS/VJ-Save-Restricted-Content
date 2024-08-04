5613f6c9e56cd59581f022e7bf4aeed125fe45e5
TJBOTS <teekam4334@gmail.com>
2024-08-04 10:17:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22349465"))
+API_ID = int(os.environ.get("API_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "3732e079c4125690226d8e7b4e028ca4")
+API_HASH = os.environ.get("API_HASH", "")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://teekam9079:9571404334@save.bespz6t.mongodb.net/?retryWrites=true&w=majority&appName=Save")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://teekam_jaat:9571404334@teekam.rkeoopf.mongodb.net/?retryWrites=true&w=majority&appName=Teekam")
